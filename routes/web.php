b4563111b5504866d517c729fda321ac7ed4c713
LeonardoPedron <leonardopedron2000@gmail.com>
2023-04-30 18:56:02 +0200

    Create AuthorController and Route (page index-show)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// <editor-folder desc="Start Route Authors">
+Route::get('/authors', [AuthorController::class, 'index']);
+// </editor-folder>
